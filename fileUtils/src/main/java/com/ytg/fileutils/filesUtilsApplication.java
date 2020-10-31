4873c8438ece396c3a4bace91fef4ba7eab1ea43
yangtiangang <651236807@qq.com>
2020-10-31 21:59:39 +0800

    init

---
+++ fileUtils/src/main/java/com/ytg/fileutils/filesUtilsApplication.java
@@ -0,0 +1,14 @@
+package com.ytg.fileutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class filesUtilsApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(filesUtilsApplication.class, args);
+    }
+
+}
